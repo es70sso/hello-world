b8ccfc4073e0073f2aca785fe58c535f4abe045e
smith <smith@smith-nb>
2017-02-21 18:38:37 +0300

    rename

---
+++ Test_GIT_2/src/app/MyApp2.java
@@ -0,0 +1,13 @@
+package app;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		System.out.println("hello git");
+		// change
+	}
+
+	public void createNewSuperFeature() {
+		System.out.println("super feature");
+	}
+}
